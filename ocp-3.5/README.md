0c085e63b07eda2ed0c86feb82f6e2e1330e6e8b
lbroudoux <laurent.broudoux@gmail.com>
2017-06-23 14:48:16 +0200

    Adding OCP 3.5 scripts

---
+++ ocp-3.5/README.md
@@ -0,0 +1,109 @@
+This work is based on https://github.com/WilliamRedHat
+
+# RedHat Openshift 3.4 cluster on Azure
+
+When creating the RedHat Openshift 3.5 cluster on Azure, you will need a SSH RSA key for access.
+Do not forget to update rhn-username, pools, etc ...
+
+## Create the cluster
+
+To have OpenShift Enterprise 3.5 running on Azure, you will have to
+follow 2 steps.
+- First deploy the cluster with one of the following method.
+- Then use Ansible to install OpenShift Container Platform 3.4
+
+### Step 1 - Create the cluster
+
+#### From the Azure Portal
+
+Click on Deploy to Azure then you will be redirected to your Azure account
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Flbroudoux%2Fopenshift-azure%2Frhel%2Focp-3.5%2Fazuredeploy.json" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
+<a href="http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2Flbroudoux%2Fopenshift-azure%2Frhel%2Focp-3.5%2Fazuredeploy.json" target="_blank">
+    <img src="http://armviz.io/visualizebutton.png"/>
+</a>
+
+Wait for the installation to be ready this will consist of having one infra node, one master and a number of nodes. Then go to the group that contains those machines and get the "OPENSHIFT MASTER SSH" command line that you will need for the next step.
+
+#### From powershell CLI
+
+```powershell
+New-AzureRmResourceGroupDeployment -Name <DeploymentName> -ResourceGroupName <RessourceGroupName> -TemplateUri https://raw.githubusercontent.com/lbroudoux/openshift-azure/rhel/ocp-3.5/azuredeploy.json
+```
+
+#### Common parameters
+
+Both methods implies the following parameters :
+
+##### Input Parameters
+
+```
+| Name          | Type          | Description                                      |   |
+| ------------- | ------------- | -------------                                    |   |
+| adminUsername | String        | Username for SSH Login and Openshift Webconsole  |   |
+| adminPassword | SecureString  | Password for the Openshift Webconsole            |   |
+| sshKeyData    | String        | Public SSH Key for the Virtual Machines          |   |
+| masterDnsName | String        | DNS Prefix for the Openshift Master / Webconsole |   |
+| numberOfNodes | Integer       | Number of Openshift Nodes to create              |   |
+| image         | String        | Operating System to use. RHEL or CentOs          |   |
+| rhnUser       | String        | Red Hat Network user id                          |   |
+| rhnPass       | SecureString  | Red Hat Network password                         |   |
+| rhnPool       | String        | Red Hat Network pool id                          |   |
+
+```
+##### Output Parameters
+
+```
+| Name| Type           | Description |
+| ------------- | ------------- | ------------- |
+| openshift Webconsole | String       | URL of the Openshift Webconsole |
+| openshift Master ssh |String | SSH String to Login at the Master |
+| openshift Router Public IP | String       | Router Public IP. Needed if you want to create your own Wildcard DNS |
+
+```
+
+You're now able to go to the next step.
+
+### Step 2 - Install Openshift with Ansible
+
+You must connect to OpenShift master using SSH with Agent forwarding. So from your local machine:
+
+```
+[username@localmachine ~]$ ssh-add
+```
+so that the SSH forwarding Agent will forward the necessary data to the script that will then install OpenShift.
+
+Then connect using your user name and the IP of master node. Example
+
+```
+[username@localmachine ~]$ ssh -A laurent@13.236.112.237
+```
+
+Then on the master you'll need to run this script :
+
+```
+[adminUsername@master ~]$ ./openshift-install.sh
+```
+The cluster is installed in 15 minutes or so. It creates 2 users (admin and demo with password redhat123). The admin user is set up as cluster admin. At the end of the script, you may be automatically logged in using admin user.
+
+## Adding NFS storage
+
+Setup scripts install a NFS server on `infranode` during the construction of Azure topology. For applications that need Persistent Volumes you will need to  run this extra script :
+
+```
+[adminUsername@master ~]$ ./create-pvs.sh
+```
+
+This is adding a bunch of Persistent volumes of different capacities for your applications.
+
+## Adding Metrics and logging
+
+If you need to demo the metrics or logging features of OpenShift, it is just easy to add them after the storage by running this extra script created during installation :
+
+```
+[adminUsername@master ~]$ ./openshift-services-deploy.sh
+```
+
+Metrics and logging features are respectively deployed into `openshift-infra` and `logging` projects after few minutes.
---
+++ ocp-3.5/master.sh
@@ -0,0 +1,322 @@
+#!/bin/bash
+# Last Modified : 2016-08-29
+
+USERNAME=$1
+PASSWORD=$2
+HOSTNAME=$3
+NODECOUNT=$4
+ROUTEREXTIP=$5
+rhn_username=$6
+rhn_pass=$7
+rhn_pool=$8
+
+
+subscription-manager register --username=${rhn_username} --password=${rhn_pass} --force
+subscription-manager attach --pool=${rhn_pool}
+
+subscription-manager repos --disable="*"
+subscription-manager repos --enable="rhel-7-server-rpms" --enable="rhel-7-server-extras-rpms" --enable="rhel-7-server-ose-3.5-rpms" --enable="rhel-7-fast-datapath-rpms"
+
+sed -i -e 's/sslverify=1/sslverify=0/' /etc/yum.repos.d/rh-cloud.repo
+sed -i -e 's/sslverify=1/sslverify=0/' /etc/yum.repos.d/rhui-load-balancers
+
+yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion docker
+yum -y install atomic-openshift-utils
+yum -y update
+
+sed -i '/OPTIONS=.*/c\OPTIONS="--selinux-enabled --insecure-registry 172.30.0.0/16"' /etc/sysconfig/docker
+
+cat <<EOF > /etc/sysconfig/docker-storage-setup
+DEVS=/dev/sdc
+VG=docker-vg
+EOF
+
+docker-storage-setup
+systemctl enable docker
+systemctl start docker
+
+yum -y install nfs-utils rpcbind
+systemctl enable rpcbind
+systemctl start rpcbind
+setsebool -P virt_sandbox_use_nfs 1
+setsebool -P virt_use_nfs 1
+
+cat <<EOF > /etc/ansible/hosts
+[OSEv3:children]
+masters
+nodes
+
+[OSEv3:vars]
+ansible_ssh_user=${USERNAME}
+ansible_become=true
+debug_level=2
+deployment_type=openshift-enterprise
+
+openshift_master_identity_providers=[{'name': 'htpasswd_auth', 'login': 'true', 'challenge': 'true', 'kind': 'HTPasswdPasswordIdentityProvider', 'filename': '/etc/origin/master/htpasswd'}]
+
+openshift_master_default_subdomain=${ROUTEREXTIP}.xip.io
+openshift_use_dnsmasq=False
+
+# Install the openshift examples
+openshift_install_examples=true
+
+# Enable cluster metrics
+use_cluster_metrics=true
+
+# Configure metricsPublicURL in the master config for cluster metrics
+openshift_master_metrics_public_url=https://metrics.${ROUTEREXTIP}.xip.io/hawkular/metrics
+
+# Configure loggingPublicURL in the master config for aggregate logging
+openshift_master_logging_public_url=https://kibana.${ROUTEREXTIP}.xip.io
+
+# Defining htpasswd users (password is redhat123)
+openshift_master_htpasswd_users={'admin': '\$apr1\$bdqbl2eo\$Na6mZ6SG7Vfo3YPyp1vJP.', 'demo': '\$apr1\$ouJ9QtwY\$Z2WZ9yvm1.tNzipdR.4Wp1'}
+
+# Enable cockpit
+osm_use_cockpit=true
+osm_cockpit_plugins=['cockpit-kubernetes']
+
+# default project node selector
+osm_default_node_selector='region=primary'
+
+openshift_router_selector='region=infra'
+openshift_registry_selector='region=infra'
+
+# Confifgure router
+# Force to 1 otherwise Ansible compute 2 replicas cause master and infranode are region=infra
+# but Ansible does not take into account that master is not schedulable. So it fails...
+openshift_hosted_router_replicas=1
+
+# Configure an internal regitry
+openshift_hosted_registry_selector='region=infra'
+openshift_hosted_registry_replicas=1
+openshift_hosted_registry_storage_kind=nfs
+openshift_hosted_registry_storage_access_modes=['ReadWriteMany']
+openshift_hosted_registry_storage_host=infranode
+openshift_hosted_registry_storage_nfs_directory=/exports
+openshift_hosted_registry_storage_volume_name=registry
+openshift_hosted_registry_storage_volume_size=15Gi
+
+# Enable metrics
+openshift_metrics_install_metrics=true
+openshift_metrics_image_prefix=openshift/openshift-
+openshift_metrics_image_version=3.5.0
+openshift_metrics_hawkular_hostname=metrics.${ROUTEREXTIP}.xip.io
+openshift_metrics_storage_kind=nfs
+openshift_metrics_storage_access_modes=['ReadWriteOnce']
+openshift_metrics_storage_host=infranode
+openshift_metrics_storage_nfs_directory=/exports
+openshift_metrics_storage_volume_name=metrics
+openshift_metrics_storage_volume_size=5Gi
+
+# Enable logging
+openshift_logging_install_logging=true
+openshift_logging_namespace=logging
+openshift_logging_use_ops=false
+openshift_logging_image_prefix=registry.access.redhat.com/openshift3/
+openshift_logging_image_version=v3.5
+openshift_logging_master_public_url=https://${HOSTNAME}:8443
+openshift_logging_es_cluster_size=1
+openshift_logging_es_pvc_size=5G
+openshift_logging_es_memory_limit=2G
+openshift_logging_es_nodeselector='region=infra'
+openshift_logging_kibana_hostname=https://kibana.${ROUTEREXTIP}.xip.io
+openshift_logging_kibana_nodeselector='region=infra'
+openshift_logging_curator_nodeselector='region=infra'
+openshift_logging_fluentd_nodeselector='region=primary'
+openshift_logging_storage_kind=nfs
+openshift_logging_storage_access_modes=['ReadWriteOnce']
+openshift_logging_storage_host=infranode
+openshift_logging_storage_nfs_directory=/exports
+openshift_logging_storage_volume_name=logging-es
+openshift_logging_storage_volume_size=10Gi
+
+[masters]
+master openshift_node_labels="{'region': 'infra', 'zone': 'default'}"  openshift_public_hostname=${HOSTNAME}
+
+[nodes]
+master
+infranode openshift_node_labels="{'region': 'infra', 'zone': 'default'}"
+node[01:${NODECOUNT}] openshift_node_labels="{'region': 'primary', 'zone': 'default'}"
+
+EOF
+
+cat <<EOF > /home/${USERNAME}/openshift-install.sh
+export ANSIBLE_HOST_KEY_CHECKING=False
+ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml
+oc annotate namespace default openshift.io/node-selector='region=infra' --overwrite
+oadm policy add-cluster-role-to-user cluster-admin admin
+EOF
+
+chmod 755 /home/${USERNAME}/openshift-install.sh
+
+n=1
+while [ $n -le 4 ]
+do
+cat <<EOF > /home/${USERNAME}/pv000$n.json
+{
+  "apiVersion": "v1",
+  "kind": "PersistentVolume",
+  "metadata": {
+    "name": "pv000$n"
+  },
+  "spec": {
+    "capacity": {
+        "storage": "1Gi"
+    },
+    "accessModes": [ "ReadWriteOnce", "ReadWriteMany" ],
+    "nfs": {
+        "path": "/exports/pv000$n",
+        "server": "infranode"
+    },
+    "persistentVolumeReclaimPolicy": "Recycle"
+  }
+}
+EOF
+(( n++ ))
+done
+
+n=5
+while [ $n -le 9 ]
+do
+cat <<EOF > /home/${USERNAME}/pv000$n.json
+{
+  "apiVersion": "v1",
+  "kind": "PersistentVolume",
+  "metadata": {
+    "name": "pv000$n"
+  },
+  "spec": {
+    "capacity": {
+        "storage": "5Gi"
+    },
+    "accessModes": [ "ReadWriteOnce", "ReadWriteMany" ],
+    "nfs": {
+        "path": "/exports/pv000$n",
+        "server": "infranode"
+    },
+    "persistentVolumeReclaimPolicy": "Recycle"
+  }
+}
+EOF
+(( n++ ))
+done
+
+n=10
+while [ $n -le 15 ]
+do
+cat <<EOF > /home/${USERNAME}/pv00$n.json
+{
+  "apiVersion": "v1",
+  "kind": "PersistentVolume",
+  "metadata": {
+    "name": "pv00$n"
+  },
+  "spec": {
+    "capacity": {
+        "storage": "10Gi"
+    },
+    "accessModes": [ "ReadWriteOnce", "ReadWriteMany" ],
+    "nfs": {
+        "path": "/exports/pv00$n",
+        "server": "infranode"
+    },
+    "persistentVolumeReclaimPolicy": "Recycle"
+  }
+}
+EOF
+(( n++ ))
+done
+
+n=16
+while [ $n -le 20 ]
+do
+cat <<EOF > /home/${USERNAME}/pv00$n.json
+{
+  "apiVersion": "v1",
+  "kind": "PersistentVolume",
+  "metadata": {
+    "name": "pv00$n"
+  },
+  "spec": {
+    "capacity": {
+        "storage": "25Gi"
+    },
+    "accessModes": [ "ReadWriteOnce", "ReadWriteMany" ],
+    "nfs": {
+        "path": "/exports/pv00$n",
+        "server": "infranode"
+    },
+    "persistentVolumeReclaimPolicy": "Recycle"
+  }
+}
+EOF
+(( n++ ))
+done
+
+cat <<EOF > /home/${USERNAME}/create-pvs.sh
+n=1
+while [ \$n -le 9 ]
+do
+  oc create -f pv000\$n.json
+  (( n++ ))
+done
+n=10
+while [ \$n -le 20 ]
+do
+oc create -f pv00\$n.json
+(( n++ ))
+done
+EOF
+
+chmod 755 /home/${USERNAME}/create-pvs.sh
+
+cat <<EOF > /home/${USERNAME}/openshift-services-deploy.sh
+
+oc project openshift-infra
+oc annotate namespace openshift-infra openshift.io/node-selector='region=infra' --overwrite
+oc create -f - <<API
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: metrics-deployer
+secrets:
+- name: metrics-deployer
+API
+oc secrets new metrics-deployer nothing=/dev/null
+oadm policy add-role-to-user edit system:serviceaccount:openshift-infra:metrics-deployer
+oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:openshift-infra:heapster
+oc adm policy add-role-to-user view system:serviceaccount:openshift-infra:hawkular -n openshift-infra
+
+oc process metrics-deployer-template -n openshift \
+  -v HAWKULAR_METRICS_HOSTNAME=metrics.${ROUTEREXTIP}.xip.io \
+  -v IMAGE_VERSION=v3.4 -v IMAGE_PREFIX=registry.access.redhat.com/openshift3/ \
+  -v USE_PERSISTENT_STORAGE=true \
+  -v CASSANDRA_PV_SIZE=5Gi \
+  | oc create -f -
+
+oc project logging
+oc new-app logging-deployer-account-template
+oadm policy add-cluster-role-to-user oauth-editor system:serviceaccount:logging:logging-deployer
+oadm policy add-scc-to-user privileged system:serviceaccount:logging:aggregated-logging-fluentd
+oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:logging:aggregated-logging-fluentd
+oadm policy add-cluster-role-to-user rolebinding-reader system:serviceaccount:logging:aggregated-logging-elasticsearch
+
+oc process logging-deployer-template -n openshift \
+  -v KIBANA_HOSTNAME=kibana.${ROUTEREXTIP}.xip.io \
+  -v ES_CLUSTER_SIZE=1 \
+  -v ES_INSTANCE_RAM=2G \
+  -v ES_PVC_SIZE=5G \
+  -v ES_NODESELECTOR='region=infra' \
+  -v KIBANA_NODESELECTOR='region=infra' \
+  -v CURATOR_NODESELECTOR='region=infra' \
+  -v PUBLIC_MASTER_URL=https://${HOSTNAME}:8443 \
+  -v ENABLE_OPS_CLUSTER=false \
+  -v IMAGE_VERSION=v3.4 \
+  -v IMAGE_PREFIX=registry.access.redhat.com/openshift3/ \
+  | oc create -f -
+
+oc label nodes --selector='region=primary' logging-infra-fluentd=true
+EOF
+
+chmod 755 /home/${USERNAME}/openshift-services-deploy.sh
